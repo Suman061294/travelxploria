41e75744855d9885fa6dcd081fe59a80c625fe2d
Suman061294 <113073120+Suman061294@users.noreply.github.com>
2024-12-04 22:52:10 +0530

    making Destination

--- next.config.js
+++ next.config.js
@@ -4,18 +4,18 @@ const nextConfig = {
     images: {
         domains: ['digi.rangrotopack.com'],
     },
-    // eslint: {
-    //     ignoreDuringBuilds: true, // Optional: Ignore ESLint errors during production builds
-    // },
-    // typescript: {
-    //     ignoreBuildErrors: true, // Optional: Ignore TypeScript errors during production builds
-    // },
-    // experimental: {
-    //     optimizeFonts: true, // Optimize font loading
-    // },
-    // env: {
-    //     API_URL: process.env.API_URL || 'https://default-api.com', // Add your environment variables
-    // },
+    eslint: {
+        ignoreDuringBuilds: true, // Optional: Ignore ESLint errors during production builds
+    },
+    typescript: {
+        ignoreBuildErrors: true, // Optional: Ignore TypeScript errors during production builds
+    },
+    experimental: {
+        optimizeFonts: true, // Optimize font loading
+    },
+    env: {
+        API_URL: process.env.API_URL || 'https://default-api.com', // Add your environment variables
+    },
 };
 
 module.exports = nextConfig;
